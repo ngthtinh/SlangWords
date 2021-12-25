8a1d6bf0bf10f939450f81f7d7f1d027f4df673f
ngthtinh <nguyenthanhtinh365@gmail.com>
2021-12-25 19:44:32 +0700

    Function 06: Xong

--- src/vn/edu/hcmus/student/_19127292/SlangWords/Function06.java
+++ src/vn/edu/hcmus/student/_19127292/SlangWords/Function06.java
@@ -1,9 +1,10 @@
 package vn.edu.hcmus.student._19127292.SlangWords;
 
 import javax.swing.*;
-import javax.swing.border.EmptyBorder;
 import java.awt.*;
+
 import java.util.HashMap;
+import javax.swing.border.EmptyBorder;
 
 /**
  * vn.edu.hcmus.student._19127292.SlangWords
@@ -12,50 +13,41 @@ import java.util.HashMap;
  * Description: Function 06 Class
  */
 public class Function06 extends JPanel {
-    JLabel slangLabel;
-    JLabel meaningLabel;
-
     public Function06(HashMap<String, String> dictionary) {
         // Find Panel
-        JTextField findTextField = new JTextField();
+        JTextField deleteTextField = new JTextField();
 
-        JButton findButton = new JButton("Find");
-        findButton.addActionListener(e -> findButtonEventHandler(dictionary, findTextField.getText()));
+        JButton deleteButton = new JButton("Delete slang");
+        deleteButton.addActionListener(e -> deleteButtonEventHandler(dictionary, deleteTextField.getText()));
 
-        JPanel findPanel = new JPanel();
-        findPanel.setLayout(new BoxLayout(findPanel, BoxLayout.X_AXIS));
+        JPanel deletePanel = new JPanel();
+        deletePanel.setLayout(new BoxLayout(deletePanel, BoxLayout.X_AXIS));
 
-        findPanel.add(findTextField);
-        findPanel.add(Box.createHorizontalStrut(5));
-        findPanel.add(findButton);
-
-        // Slang Panel
-        slangLabel = new JLabel();
-        slangLabel.setFont(new Font("Arial", Font.PLAIN, 30));
-        slangLabel.setForeground(Color.BLUE);
-
-        meaningLabel = new JLabel();
-
-        JPanel slangPanel = new JPanel();
-        slangPanel.setLayout(new BoxLayout(slangPanel, BoxLayout.Y_AXIS));
-        slangPanel.setBorder(new EmptyBorder(5, 5, 5, 5));
-
-        slangPanel.add(slangLabel);
-        slangPanel.add(meaningLabel);
+        deletePanel.add(deleteTextField);
+        deletePanel.add(Box.createHorizontalStrut(5));
+        deletePanel.add(deleteButton);
 
         // Settings and Add components to panel
         setBorder(new EmptyBorder(5, 5, 0, 5));
         setLayout(new BorderLayout());
 
-        add(findPanel, BorderLayout.PAGE_START);
-        add(slangPanel, BorderLayout.CENTER);
+        add(deletePanel, BorderLayout.PAGE_START);
     }
 
-    void findButtonEventHandler(HashMap<String, String> dictionary, String str) {
+    void deleteButtonEventHandler(HashMap<String, String> dictionary, String str) {
         String ans = dictionary.get(str);
-        slangLabel.setText(ans != null ? str : "No slang!");
-        meaningLabel.setText(ans != null ? "➥ " + ans : "Try another slang!");
 
-        if (ans != null) Function03.addHistory(str);
+        if (ans != null) {
+            int choice = JOptionPane.showConfirmDialog(
+                    this,
+                    "Slang: " + str + "\nMeanng: " + ans +
+                            "\nDo you really want to delete this slang?",
+                    "Delete slang",
+                    JOptionPane.YES_NO_OPTION);
+            if (choice == 0) {
+                dictionary.remove(str);
+                JOptionPane.showMessageDialog(this, "Deleted: " + str);
+            }
+        } else JOptionPane.showMessageDialog(this, "No slang: " + str + " to delete.");
     }
 }
